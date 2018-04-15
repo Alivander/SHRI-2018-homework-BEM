f9c116658c86915c54681d15cbf641142015f994
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-15 15:32:57 +0300

    adds desctop redefinition level

--- src/desktop/Image/Image.js
+++ src/desktop/Image/Image.js
@@ -11,8 +11,10 @@ export default decl({
 
     return (
       <Fragment>
+        <source media='(min-width: 1200px)' srcSet={`${src}@3x.webp`} type='image/webp'/>
         <source media='(min-width: 512px)' srcSet={`${src}@2x.webp`} type='image/webp'/>
         <source srcSet={`${src}.webp`} type='image/webp'/>
+        <source media='(min-width: 1056px)' srcSet={`${src}@3x${format}`}/>
         <source media='(min-width: 704px)' srcSet={`${src}@2x${format}`}/>
         <img src={`${src}${format}`} alt={alt} srcSet={`${src}@2x${format} 2x`}/>
       </Fragment>

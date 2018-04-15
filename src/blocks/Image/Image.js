3ccfd12bf22574d5ac694e72cb9a754e4254737e
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-15 14:35:31 +0300

    corrects Image block

--- src/blocks/Image/Image.js
+++ src/blocks/Image/Image.js
@@ -4,19 +4,19 @@ import { decl } from 'bem-react-core';
 export default decl({
   block: 'Image',
   tag: 'picture',
-  content({title, image}) {
+  content({alt, image}) {
     image = `server-stub/${image}`;
     const src = image.substring(0, image.length - 4);
     const format = image.substring(image.length - 4);
 
     return (
       <Fragment>
-        <source media='(min-width: 1200px)' srcset={`${src}@3x.webp`} type='image/webp'/>
-        <source media='(min-width: 512px)' srcset={`${src}@2x.webp`} type='image/webp'/>
-        <source srcset={`${src}.webp`} type='image/webp'/>
-        <source media='(min-width: 1056px)' srcset={`${src}@3x${format}`}/>
-        <source media='(min-width: 704px)' srcset={`${src}@2x${format}`}/>
-        <img src={`${src}${format}`} alt={title} srcset={`${src}@2x${format} 2x`}/>
+        <source media='(min-width: 1200px)' srcSet={`${src}@3x.webp`} type='image/webp'/>
+        <source media='(min-width: 512px)' srcSet={`${src}@2x.webp`} type='image/webp'/>
+        <source srcSet={`${src}.webp`} type='image/webp'/>
+        <source media='(min-width: 1056px)' srcSet={`${src}@3x${format}`}/>
+        <source media='(min-width: 704px)' srcSet={`${src}@2x${format}`}/>
+        <img src={`${src}${format}`} alt={alt} srcSet={`${src}@2x${format} 2x`}/>
       </Fragment>
     );
   }

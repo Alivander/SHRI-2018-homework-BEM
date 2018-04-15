5a1acddf394a84208ba7e0d0adc1c508b4bcfa0a
Alina Vanieva <vanieva.alina@yandex.ru>
2018-04-15 14:35:03 +0300

    corrects conteiner

--- src/blocks/Content/Container/Content-Container.js
+++ src/blocks/Content/Container/Content-Container.js
@@ -8,12 +8,21 @@ import dataStub from '../../../server-stub/data';
 export default decl({
   block: 'Content',
   elem: 'Container',
-  tag: 'main',
+  tag: 'section',
   state: {
     data: dataStub
   },
   content() {
     const data = this.state.data;
-    return data.map((item) => <Card {...item}/>);
+    return data.map((item) => {
+      if (item.size === 's') {
+        if (item.image) {
+          item.content = 'image';
+        } else {
+          item.content = 'text';
+        }
+      }
+      return <Card {...item}/>;
+    });
   }
 });
